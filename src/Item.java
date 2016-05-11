fc81e25f2a20cf557895da7769c57ceb896f86af
Saffat Akanda <jadenandyubel@hotmail.com>
2016-05-11 15:16:09 +1000

    Added equals and hashcode to Item

--- src/Item.java
+++ src/Item.java
@@ -34,7 +34,27 @@ public class Item {
 	public void setPos(Point pos) {
 		this.pos = pos;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (itemName != other.itemName)
+			return false;
+		return true;
+	}
 	
 }
