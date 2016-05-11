56e47ef17662276acc1b28dd3259fc20a35a3de9
Saffat Akanda <jadenandyubel@hotmail.com>
2016-05-11 15:14:18 +1000

    Get gold partially implemented,
    Constructor same as explore and getItem
    If it can see the gold it will return that as the goal otherwise if it has the gold it will return to the start
    for debugging purposes all other times it will return the start as the goal node
    Heuristic uses manhatten distance between current position and goal position

--- src/GetGold.java
+++ src/GetGold.java
@@ -1,11 +1,37 @@
+import java.awt.Point;
 import java.util.ArrayList;
 
 public class GetGold implements Behaviour{
 
+	private Graph map;
+	private ArrayList<Item> inventory;
+	private Point startPoint;
+	
+	public GetGold(Graph map, ArrayList<Item> inv, Point startPoint) {
+		this.startPoint = startPoint;
+		this.map = map;
+		this.inventory = inv;
+	}
+	
+	
 	@Override
-	public int returnHeuristic(Graph map, ArrayList<Item> inventory) {
-		// TODO Auto-generated method stub
-		return 0;
+	public Point getGoal() {
+		for (Item i: map.getItems()) {
+			if (i.getItemName() == Tile.Gold)
+				return i.getPos();
+		}
+		for (Item i : inventory) {
+			if (i.getItemName() == Tile.Gold)
+				return startPoint;
+		}
+		
+		return startPoint;
+	}
+	
+	@Override
+	public int returnHeuristic(Point goal, Point currPosition) {
+		int h = Math.abs(goal.x - currPosition.x) + Math.abs(goal.y - currPosition.y);
+		return (h < 0) ? -h : h;
 	}
 
 }
