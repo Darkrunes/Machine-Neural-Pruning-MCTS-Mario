fe84118fb9ccc0ad754941e8ec7311416adbac1b
Saffat Akanda <jadenandyubel@hotmail.com>
2016-05-11 15:12:36 +1000

    All behaviours now also store the map and player inventory has private fields rather then having them passed in multiple times
    Reason:
    The states did not have access to the goal and the Astar did not have access to the current player inventory

--- src/Explore.java
+++ src/Explore.java
@@ -1,9 +1,23 @@
+import java.awt.Point;
 import java.util.ArrayList;
 
 public class Explore implements Behaviour{
+	
+	private Graph map;
+	private ArrayList<Item> inventory;
+	
+	public Explore(Graph map, ArrayList<Item> inv) {
+		this.map = map;
+		this.inventory = inv;
+	}
+	
+	@Override
+	public Point getGoal() {
+		return null;
+	}
 
 	@Override
-	public int returnHeuristic(Graph map, ArrayList<Item> inventory) {
+	public int returnHeuristic(Point goal, Point currentPosition) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
--- src/GetItem.java
+++ src/GetItem.java
@@ -1,11 +1,26 @@
+import java.awt.Point;
 import java.util.ArrayList;
 
 public class GetItem implements Behaviour{
 
+	private Graph map;
+	private ArrayList<Item> inventory;
+	
+	public GetItem(Graph map, ArrayList<Item> inv) {
+		this.map = map;
+		this.inventory = inv;
+	}
+	
+	
 	@Override
-	public int returnHeuristic(Graph map, ArrayList<Item> inventory) {
+	public Point getGoal() {
+		return null;
+	}
+
+
+	@Override
+	public int returnHeuristic(Point goal, Point currentPosition) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 }
