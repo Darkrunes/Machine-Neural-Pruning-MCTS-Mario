631bb4d02d9aa15f8df9ceb0caa0c9f9e49d04b3
Saffat Akanda <jadenandyubel@hotmail.com>
2016-05-21 23:43:27 +1000

    Added javadoc to Behavior and State

--- src/Behaviour.java
+++ src/Behaviour.java
@@ -1,6 +1,10 @@
 import java.awt.Point;
 import java.util.ArrayList;
 
+/**
+ * Behaviors represent the different modes of the Agent 
+ * @author Saffat Shams Akanda, Richard Luong
+ */
 public interface Behaviour {
 	public Point getGoal();
 	public int returnHeuristic(Point goal, Point currentPosition);
